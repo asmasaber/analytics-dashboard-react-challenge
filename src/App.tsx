201cfc412bb86f16655de41e428537b3adaa328e
Asma <asmaasaber@Asmaas-MacBook-Pro.local>
2022-09-09 14:08:48 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <h1>Analysis Chart Front-End Challenge</h1>;
+};
+
+export default App;
---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('App component', function () {
+  test('should render without errors', () => {
+    render(<App />);
+    const headerElement = screen.getByText(/Analysis Chart Front-End Challenge/i);
+    expect(headerElement).toBeDefined();
+    expect(headerElement).not.toBeNull();
+    expect(headerElement).toBeInTheDocument();
+  });
+});
